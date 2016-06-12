0edb14e72529eb9ca421d72a2762090eb0f1fc2d
mpkocher <michael.kocher@me.com>
2016-06-12 11:42:38 -0700

    Update TC/RTC python example

--- hello_world_quick.py
+++ hello_world_quick.py
@@ -3,31 +3,54 @@
 import sys
 import logging
 
-from pbcommand.models import FileTypes, OutputFileType
+from pbcommand.models import FileTypes
 from pbcommand.cli import registry_builder, registry_runner
 
 log = logging.getLogger(__name__)
 
-# the 'Driver' exe needs to be your your path
-registry = registry_builder("pbsmrtpipe_examples", "hello_world_quick.py ")
+NAMESPACE = "pbsmrtpipe_examples"
+
+# the 'Driver' exe needs to be your your path. The first arg will be the path
+# to the resolved tool contract.
+#
+# Note, When the tool contract is emitted, the 'run-rtc'
+# will automatically be added to the driver.
+#
+# When this commandline tool is invoked, it will be of the form:
+# hello_world_quick.py run-rtc /path/to/resolved-tool-contract.py
+registry = registry_builder(NAMESPACE, "hello_world_quick.py ")
 
 
-def _example_main(input_files, output_files, **kwargs):
-    # This func should be imported from your python package. 
+def _example_main(input_file, output_file, **kwargs):
+    """
+    This func should be imported from your python package.
+
+    This should have *no* dependency on the pbcommand IO, such as the RTC/TC models.
+    """
+
     # This is just for test purposes
-    log.info("Running example main with {i} {o} kw:{k}".format(i=input_files,
-                                                               o=output_files, k=kwargs))
-    # write mock output files, otherwise the End-to-End test will fail
-    xs = output_files if isinstance(output_files, (list, tuple)) else [output_files]
-    for x in xs:
-        with open(x, 'w') as writer:
-            writer.write("Mock data\n")
+    log.info("Running example main with {i} {o} kw:{k}".format(i=input_file,
+                                                               o=output_file,
+                                                               k=kwargs))
+
+    # write mock output files, otherwise the End-to-End test will fail when
+    # run within testkit
+    with open(output_file, 'w') as f:
+        f.write("MOCK TEST DATA")
     return 0
 
 
-@registry("dev_mk_example_txt", "0.2.1", FileTypes.TXT, FileTypes.TXT, nproc=1, options=dict(alpha=1234))
+@registry("dev_mk_example_txt", "0.2.2", (FileTypes.TXT, ), (FileTypes.TXT, ), nproc=1, options=dict(alpha=1234))
 def run_rtc(rtc):
-    return _example_main(rtc.task.input_files[0], rtc.task.output_files[0], nproc=rtc.task.nproc)
+    """Example Task for testing. Takes a txt file as input and writes a txt file with mock data"""
+    # The above docstring will be used as the Task/ToolContract Description
+
+    log.info("Got RTC task options {t}".format(t=rtc.task.options))
+    log.info("Got nproc {n}".format(n=rtc.task.nproc))
+
+    # The Task options are now accessible via global identifier
+    alpha = rtc.task.options['pbsmrtpipe_examples.task_options.alpha']
+    return _example_main(rtc.task.input_files[0], rtc.task.output_files[0], nproc=rtc.task.nproc, alpha=alpha)
 
 
 if __name__ == '__main__':
